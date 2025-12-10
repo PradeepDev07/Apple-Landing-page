a147544961a1d22bb35fd975b467b1d3bf4e4e0b
PradeepDev07 <pradeep.m.developer@gmail.com>
2025-12-10 17:26:09 +0530

    ADD:implemented Product Section v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
   return (
     <main className="">
       <Navbar />
       <Hero/>
+      <ProductViewer/>
 
       </main>
   )
