ffa68842de583423881292693c6b333749914afb
PradeepDev07 <pradeep.m.developer@gmail.com>
2025-12-11 14:02:05 +0530

    ADD:implimented most of the section with there animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import ProductViewer from "./components/ProductViewer"
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
-  return (
-    <main className="">
-      <Navbar />
-      <Hero/>
-      <ProductViewer/>
-
-      </main>
-  )
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
 }
 
 export default App
